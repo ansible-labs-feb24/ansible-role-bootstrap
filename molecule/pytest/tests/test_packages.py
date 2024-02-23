2f5f13f469bb854435864180090bbb3ce9b7c39a
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2024-02-22 20:36:34 -0600

    Wrote tests to check for python3-cryptography package

--- molecule/pytest/tests/test_packages.py
+++ molecule/pytest/tests/test_packages.py
@@ -2,6 +2,7 @@ import pytest
 # Confirm that specific packages and versions are installed
 @pytest.mark.parametrize("name,version", [
     ("python3-psutil", "5"),
+    ("python3-cryptography", "3.2"),
 ])
 def test_packages(host, name, version):
     pkg = host.package(name)
